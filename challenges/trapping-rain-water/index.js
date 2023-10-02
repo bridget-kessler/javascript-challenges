c27d9011f632d39bced2d38e5e5b5bb3806ba9a4
Bridget Kessler <106347277+bridget-kessler@users.noreply.github.com>
2023-10-02 18:44:12 -0500

    Refactor to handle edge case with negative integers

--- challenges/trapping-rain-water/index.js
+++ challenges/trapping-rain-water/index.js
@@ -10,26 +10,32 @@ const trap = function(height) {
     let rightMax = 0;
     let sum = 0;
 
-    for (let i = 0; i < height.length; i++) {
+    for (let i = 0; i < height?.length; i++) {
         if (height[i] > leftMax) {
             leftMax = height[i];
         }
-        left[i] = leftMax
+        left[i] = leftMax;
     }
 
-    for (let i = height.length; i >= 0; i--) {
+    for (let i = height?.length - 1; i >= 0; i--) {
         if (height[i] > rightMax) {
             rightMax = height[i];
         }
         right[i] = rightMax;
     }
 
-    for (let i = 0; i < height.length; i++) {
-        sum += Math.min(left[i], right[i]) - height[i];
+    for (let i = 0; i < height?.length; i++) {
+        const elevation = height[i] < 0 ? 0 : height[i];
+
+        if (left[i] > 0 && right[i] > 0) {
+            sum += Math.min(left[i], right[i]) - elevation;
+        }
     }
 
     return sum;       
 }
 
-console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1])) // 6
-console.log(trap([4,2,0,3,2,5])) // 9
+// // console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1])) // 6
+trap([5, 3, 0, -40, 2, 10, -10, 0]) // 9
+
+module.exports = trap;
